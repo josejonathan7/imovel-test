75b223ca668b0fab92e4facd9a78e1f738856cea
José Jonathan <josejonathan689@gmail.com>
2021-12-19 23:20:16 -0300

    conclusao de mais da metade dos testes unitarios de produtos

--- __tests__/unit/product.test.ts
+++ __tests__/unit/product.test.ts
@@ -1,5 +1,7 @@
-import { describe, it, expect, beforeAll } from "@jest/globals";
+import { describe, it, expect, beforeAll, afterEach } from "@jest/globals";
 import faker from "faker";
+import { getCustomRepository } from "typeorm";
+import { ProductRepositorie } from "../../src/repositories/productRepositorie";
 import { ProductService } from "../../src/services/productService";
 
 describe("#Test product unit functions", () => {
@@ -9,6 +11,7 @@ describe("#Test product unit functions", () => {
 		image: string;
 		description: string;
 		price: number;
+		category: string;
 	};
 
 	beforeAll(() => {
@@ -18,13 +21,147 @@ describe("#Test product unit functions", () => {
 			name: faker.commerce.productName(),
 			image: faker.image.transport(),
 			description: faker.commerce.productDescription(),
-			price: Number(faker.commerce.price())
+			price: Number(faker.commerce.price()),
+			category: "armario"
 		};
 	});
 
-	it("-> call", async () => {
+	afterEach(async () => {
+		const productRepositorie = getCustomRepository(ProductRepositorie, "tests");
 
-		const ok = await product.createProduct(data);
-		expect(ok).toBe(true);
+		await productRepositorie.delete({});
+	});
+
+	it("->Should create product", async () => {
+		const createProduct = await product.createProduct(data);
+
+		expect(createProduct).toMatchObject({
+			...data,
+			category: {
+			}
+		});
+	});
+
+	it("->Should not create existent product", async () => {
+		await product.createProduct(data);
+
+		try {
+			await product.createProduct(data);
+
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			expect(error.message).toBe("This product is already registered");
+		}
+	});
+
+	it("->Should update product", async () => {
+		const createProduct = await product.createProduct(data);
+		const id = createProduct.id;
+
+		const updatedData = {
+			name: faker.commerce.productName(),
+			image: faker.image.transport(),
+			description: faker.commerce.productDescription(),
+			price: Number(faker.commerce.price()),
+			category: "quarto"
+		};
+
+		const updateProduct = await product.updateProduct({...updatedData}, id);
+
+		expect(updateProduct).toBeTruthy();
+	});
+
+	it("->Should not update product with invalid Id", async () => {
+		await product.createProduct(data);
+
+		const updatedData = {
+			name: faker.commerce.productName(),
+			image: faker.image.transport(),
+			description: faker.commerce.productDescription(),
+			price: Number(faker.commerce.price()),
+			category: "quarto"
+		};
+
+
+
+		try {
+			await product.updateProduct({...updatedData}, "id");
+
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			expect(error.message).toBe("Not found a record to update");
+		}
+	});
+
+	it("->Should delete product", async () => {
+		const createProduct = await product.createProduct({...data});
+
+		const id = createProduct.id;
+
+		const deleteProduct = await product.deleteProduct(id);
+
+		expect(deleteProduct).toBeTruthy();
+	});
+
+	it("->Should not delete with invalid Id", async () => {
+		await product.createProduct({...data});
+
+		try {
+			await product.deleteProduct("id");
+
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			expect(error.message).toBe("Not found register to delete");
+		}
+	});
+
+	it("->Should list all products", async () => {
+		await product.createProduct(data);
+
+		const productTwo = {
+			...data,
+			name: faker.commerce.productName()
+		};
+
+		await product.createProduct(productTwo);
+
+		const productThree = {
+			...data,
+			name: faker.commerce.productName()
+		};
+
+		await product.createProduct(productThree);
+
+		const listProducts = await product.getAllProducts();
+
+		expect(listProducts[0]).toMatchObject({...data, category: {}});
+	});
+
+	it("->Should get all product where category is defined", async () => {
+		await product.createProduct(data);
+
+		const productTwo = {
+			...data,
+			name: faker.commerce.productName(),
+			category: "quarto"
+		};
+
+		await product.createProduct(productTwo);
+
+		const productThree = {
+			...data,
+			name: faker.commerce.productName()
+		};
+
+		await product.createProduct(productThree);
+
+		const getProductByCategoryResponse = await product.getProductByCategory("armario");
+
+		expect(getProductByCategoryResponse[0]).toMatchObject({
+			...data,
+			category: {
+				name: "armario"
+			}
+		});
 	});
 });
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -8,19 +8,28 @@ interface ProductType {
 	image: string;
 	description: string;
 	price: number;
+	category: string;
 }
 
 export class ProductService {
 	connectionName: string;
 	connection: Promise<Connection>;
+	wardrobe: string;
+	bedroom: string;
 
 	constructor() {
-		dotenv.config();
+		dotenv.config({
+			path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+		});
+
 		this.connectionName = process.env.NODE_ENV === "test" ? "tests" : "";
 		this.connection = Promise.resolve(this.load()) as Promise<Connection>;
+
+		this.wardrobe = process.env.WARDROBE as string;
+		this.bedroom = process.env.BEDROOM as string;
 	}
 
-	async createProduct(data: ProductType){
+	async createProduct(data: ProductType) {
 		const productRepositorie = await this.connection.then(con => con.getCustomRepository(ProductRepositorie));
 
 		const verifyExists = await productRepositorie.findOne({
@@ -31,16 +40,89 @@ export class ProductService {
 			throw new Error("This product is already registered");
 		}
 
+		const categoryId = data.category === "armario" ? this.wardrobe : this.bedroom;
+
 		const product = productRepositorie.create({
 			...data,
 			category: {
-				name: "armario"
+				id: categoryId
 			}
 		});
 
 		const response = await productRepositorie.save(product);
 
 		return instanceToPlain(response);
+	}
+
+	async updateProduct({ name, image, description, price, category}: ProductType, id: string) {
+		const productRepositorie = await this.connection.then(con => con.getCustomRepository(ProductRepositorie));
+
+		const categoryId = category === "armario" ? this.wardrobe : this.bedroom;
+
+		const updateProduct = await productRepositorie.update(id, {
+			name,
+			image,
+			description,
+			price,
+			category: {
+				id: categoryId
+			}
+		});
+
+		const affected: number = updateProduct.affected ? updateProduct.affected : 0;
+
+		if(affected === 0) {
+			throw new Error("Not found a record to update");
+		}
+
+		return true;
+	}
+
+	async deleteProduct(id: string) {
+		const productRepositorie = await this.connection.then(con => con.getCustomRepository(ProductRepositorie));
+
+		const deleteProduct = await productRepositorie.delete(id);
+
+		const affected: number = deleteProduct.affected ? deleteProduct.affected : 0;
+
+		if(affected === 0) {
+			throw new Error("Not found register to delete");
+		}
+
+		return true;
+	}
+
+	async getAllProducts() {
+		const productRepositorie = await this.connection.then(con => con.getCustomRepository(ProductRepositorie));
+
+		const productList = await productRepositorie.find();
+
+		if(!productList) {
+			throw new Error("Not have products registers");
+		}
+
+		return instanceToPlain(productList);
+	}
+
+	async getProductByCategory(category: string) {
+		const productRepositorie = await this.connection.then(con => con.getCustomRepository(ProductRepositorie));
+
+		const categoryId = category === "quarto" ? this.bedroom : category === "armario" ? this.wardrobe : "cozinha";
+
+		const products = await productRepositorie.find({
+			where: {
+				category: {
+					id: categoryId
+				}
+			}
+		});
+
+		if(!products) {
+			throw new Error("Not have products in the category search");
+		}
+
+		return instanceToPlain(products);
+
 	}
 
 	async load() {
